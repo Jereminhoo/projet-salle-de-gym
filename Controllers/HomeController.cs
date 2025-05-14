3900f291581c1bbeed0810dc7ee4dc3d676ded05
Jeremy <jeremy.muanza@condorcet.be>
2025-05-14 14:12:57 +0200

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Projet.Models;
+using Projet.Data;
+using System.Diagnostics;
+
+namespace Projet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var produits = _context.Produits.ToList(); // récupère les produits de la BDD
+            return View(produits);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Produit.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projet.Models
+{
+    [Table("produit")]
+    public class Produit
+
+    {
+        [Key]
+        [Column("id_prod")]
+        public int Id { get; set; }
+
+        public string nom_produit { get; set; }
+        public string description { get; set; }
+        public decimal prix { get; set; }
+        public int stock { get; set; }
+        public int id_cat { get; set; }
+        public string photo { get; set; }
+    }
+}
